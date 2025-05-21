e63fbd7aa19554915f7096d8732ecaa61a5621a4
muffindud <ccatlabuga2002@gmail.com>
2025-05-21 14:37:08 +0300

    add internal auth

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+from datetime import timedelta, datetime, timezone
 from os import environ
+from json import dumps
 
 from httpx import AsyncClient
-
-from json import dumps
+from jwt import encode
 
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
@@ -10,6 +11,23 @@ from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 SERVER_URI = f"{environ.get("SERVER_HOST")}:{environ.get("SERVER_PORT")}"
 INTERNAL_SECRET = environ.get("INTERNAL_SECRET")
+TOKEN_EXPIRATION = timedelta(minutes=3)
+
+
+def generate_jwt(user_credentials: dict[str, str]) -> str:
+    payload = {
+        "TelegramId": user_credentials["TelegramId"],
+        "TelegramUsername": user_credentials["TelegramUsername"],
+        "exp": datetime.now(timezone.utc) + TOKEN_EXPIRATION,
+    }
+
+    token = encode(
+        payload=payload,
+        key=INTERNAL_SECRET,
+        algorithm="HS256"
+    )
+
+    return token
 
 
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -24,7 +42,7 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
             url=f"http://{SERVER_URI}/api/user",
             json=user_credentials,
             headers={
-                # "Authorization": f"Bearer {environ.get('API_KEY')}",
+                "Authorization": f"Bearer {generate_jwt(user_credentials)}",
                 "Content-Type": "application/json",
             },
         )
